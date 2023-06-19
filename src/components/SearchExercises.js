0d767c26bf2dd086f25a5e03535752a16c5bf722
valensixx <valensixx@gmail.com>
2023-06-19 15:37:13 +0300

    woring on SearchData.js locics

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,6 +3,13 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState('')
+  const handleSearch = async () => {
+    if(search){
+     // const exercisesData = await fetchData();
+    }
+  }
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
@@ -12,13 +19,13 @@ const SearchExercises = () => {
         <TextField
           sx ={{
             input: {fontWeight: '700', border: 'none', borderRadius: '4px'},
-            width: {lg: '1170px', xs: '350px'},
+            width: {lg: '800px', xs: '350px'},
             backgroundColor: '#fff', 
             borderRadius: '40px',
           }}
           height="76px"
-          value=""
-          onChange={(e) => { }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
           placeholder="Search Exrcises"
           type="text"
         />
@@ -31,7 +38,9 @@ const SearchExercises = () => {
             fontSize: {lg: '20px', xs: '14px'},
             height: '56px', 
             position: 'absolute',
+            right: '0',
           }}
+          onClick={handleSearch}
         >
           Search
         </Button>
