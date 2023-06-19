4e144b41169123c62bda54f0f0adc42b5687e9db
valensixx <valensixx@gmail.com>
2023-06-19 16:16:58 +0300

    fixed errors

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,43 +3,59 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exercisesOptions, fetchData } from '../utils/fetchData';
 
 const SearchExercises = () => {
-  const [search, setSearch] = useState('')
-  const handleSearch = async () => {
-    if(search){
-     const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises',
-     exercisesOptions);
+  const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
 
-     console.log(exercisesData);
+  //useEffect 
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises',
+        exercisesOptions
+      );
+
+      const searchedExercises = exercisesData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search.toLowerCase()) ||
+          exercise.target.toLowerCase().includes(search.toLowerCase()) ||
+          exercise.equipment.toLowerCase().includes(search.toLowerCase()) ||
+          exercise.bodyPart.toLowerCase().includes(search.toLowerCase())
+      );
+
+      setSearch('');
+      setExercises(searchedExercises);
     }
-  }
+  };
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
-        Awesome Exercise You <br /> Shoud Know
+        Awesome Exercise You <br /> Should Know
       </Typography>
       <Box position="relative" mb="72px">
         <TextField
-          sx ={{
-            input: {fontWeight: '700', border: 'none', borderRadius: '4px'},
-            width: {lg: '800px', xs: '350px'},
-            backgroundColor: '#fff', 
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
             borderRadius: '40px',
           }}
           height="76px"
           value={search}
-          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
-          placeholder="Search Exrcises"
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
           type="text"
         />
-        <Button className="search-btn"
-          sx ={{
-            bgcolor: '#FF2625', 
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: '#FF2625',
             color: '#fff',
             textTransform: 'none',
-            width: {lg: '175', xs: '80px'},
-            fontSize: {lg: '20px', xs: '14px'},
-            height: '56px', 
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
             position: 'absolute',
             right: '0',
           }}
@@ -49,7 +65,7 @@ const SearchExercises = () => {
         </Button>
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
