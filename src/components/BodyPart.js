dc2ae50bc4c460f8b3c095b5acf8a26130cfa5fb
valensixx <valensixx@gmail.com>
2023-06-23 14:07:17 +0300

    Working on bodypart component

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Stack, Typography } from '@mui/material';
+import Icon from '../assets/icons/gym.png';
 
-const BodyPart = () => {
+const BodyPart = ({item, setBodyPart, bodyPart}) => {
   return (
-    <div>BodyPart</div>
+   <Stack 
+        type="button"
+        alignItems="center"
+        justifyContent="center"
+        className="bodyPart-card"
+        sx = {{
+                borderTop: bodyPart === item ? '4px solid #FF2625' : '', 
+                borderTop: '4px solid #FF2625',
+                background: '#fff',
+                borderBottomLeftRadius: '20px',
+                width: '270px',
+                height: '280px',
+                cursor: 'pointer',
+                gap: '47px'
+            }}
+             
+   >
+        <img src = {Icon} alt = "dumbbell" styles= {{width: '40px', height: '40px'}} />
+   </Stack>
   )
 }
 
