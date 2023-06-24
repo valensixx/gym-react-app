2b55ddfd20017ca489696636d1523db713961ce3
valensixx <valensixx@gmail.com>
2023-06-24 15:15:31 +0300

    fixed errors in home page component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,12 +11,14 @@ const Home = () => {
     <Box>
         <HeroBanner />
         <SearchExercises 
-          setExercises={setExercises} 
-          bodyPart={bodyPart} setBodyPart={setBodyPart}
+         setExercises={setExercises} 
+         bodyPart={bodyPart} 
+         setBodyPart={setBodyPart}
         />
         <Exercises 
+          exercises={exercises}
           setExercises={setExercises} 
-          bodyPart={bodyPart} setBodyPart={setBodyPart}
+          bodyPart={bodyPart} 
         />
     </Box>
   )
