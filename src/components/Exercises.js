dc67d2a034937006d6a8003a98583a9b6eb082aa
valensixx <valensixx@gmail.com>
2023-06-24 15:10:48 +0300

    working on exercises component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,6 +1,9 @@
-import React from 'react'
-
-const Exercises = () => {
+import React, {useEffect, useState} from 'react';
+import  Pagination  from '@mui/material/Pagination';
+import {Box, Stack, Typography} from '@mui/material';
+import {exerciseOptions, fetchData} from '../utils/fetchData';
+ 
+const Exercises = ({exercises, setExercises, bodyPart}) => {
   return (
     <div>Exercises</div>
   )
--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,14 +1,13 @@
-import React, {useContext} from 'react';
-import {Box, Typography} from '@mui/material';
-import BodyPart from './BodyPart';
+import React, { useContext } from 'react';
+import { Box, Typography } from '@mui/material';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import ExerciseCard from './ExerciseCard';
 
 const LeftArrow = () => {
-const { scrollPrev } = useContext(VisibilityContext);
+  const { scrollPrev } = useContext(VisibilityContext);
 
   return (
     <Typography onClick={() => scrollPrev()} className="right-arrow">
@@ -18,7 +17,7 @@ const { scrollPrev } = useContext(VisibilityContext);
 };
 
 const RightArrow = () => {
-const { scrollNext } = useContext(VisibilityContext);
+  const { scrollNext } = useContext(VisibilityContext);
 
   return (
     <Typography onClick={() => scrollNext()} className="left-arrow">
@@ -30,13 +29,8 @@ const { scrollNext } = useContext(VisibilityContext);
 const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
     {data.map((item) => (
-      <Box
-        key={item.id || item}
-        itemId={item.id || item}
-        title={item.id || item}
-        m="0 40px"
-      >
-        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+      <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m="0 40px">
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} />}
       </Box>
     ))}
   </ScrollMenu>
