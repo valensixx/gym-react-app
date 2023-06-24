7313d5643993a3386b30483434a065558b40dcc6
valensixx <valensixx@gmail.com>
2023-06-24 15:44:11 +0300

    working on exercise.js component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -5,7 +5,24 @@ import {exerciseOptions, fetchData} from '../utils/fetchData';
  
 const Exercises = ({exercises, setExercises, bodyPart}) => {
   return (
-    <div>Exercises</div>
+    <Box id= "exercises"
+      sx = {{mt: {lg:'110px'}}}
+      mt = '50px'
+      p = '20px'
+    >
+      <Typography variant='h3' mb='46px'>
+        Showing Results
+      </Typography>
+      <Stack direction='row'
+        sx={{gap: {lg: '110px', sx: '50px'}}}
+        flexWrap='wrap'
+        justifyContent='center'
+      >
+        {exercises.map((exercise, index)=> (
+          <p> exercise.name </p>
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
