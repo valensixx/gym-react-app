f689b2ee08d2d8584e624ff4600ed279d1690112
valensixx <valensixx@gmail.com>
2023-06-25 14:36:10 +0300

    fixing errors and add some style changes

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import  Pagination  from '@mui/material/Pagination';
 import {Box, Stack, Typography} from '@mui/material';
-import {exerciseOptions, fetchData} from '../utils/fetchData';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
  
 const Exercises = ({exercises, setExercises, bodyPart}) => {
@@ -9,28 +9,33 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisePerPage = 9;
 
-  const indexOfLastExercise = currentPage * exercisePerPage;
-  const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
 
-  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
-
-  const paginate = (e, value) => {
-    currentPage = value;
-    window.scrollTo({ top: 1800, behavior:'smooth' })
-  };
 
   useEffect(()=> {
     const fetchExercisesData = async () => {
       let exercisesData = [];
+
       if (bodyPart === 'all') {
         exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
       } else {
         exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
       }
       setExercises(exercisesData);
-    }
+    };
+
     fetchExercisesData();
-  }, [bodyPart])
+
+  }, [bodyPart]);
+
+  //pagination part
+  const indexOfLastExercise = currentPage * exercisePerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: 'smooth' });
+  };
 
   return (
     <Box id= "exercises"
@@ -38,7 +43,7 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
       mt = '50px'
       p = '20px'
     >
-      <Typography variant='h3' mb='46px'>
+      <Typography variant='h3' mb='46px' fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }}>
         Showing Results
       </Typography>
       <Stack direction='row'
@@ -50,13 +55,13 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
           <ExerciseCard key={index} exercise={exercise} /> 
         ))}
       </Stack>
-      <Stack mt='100px' alignItems='center'>
+      <Stack mt='100px' alignItems='center' sx={{ mt: { lg: '114px', xs: '70px' } }}>
           {exercises.lenght > 9 && (
             <Pagination 
               color='standard'
               shape='rounded'
               defaultPage={1}
-              count={Math.ceil(exercises.lenght / exercisePerPage)}
+              count={Math.ceil(exercises.length / exercisePerPage)}
               page={currentPage}
               onChange={paginate}
               size='large'
@@ -64,7 +69,7 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
           )}
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default Exercises
+export default Exercises;
