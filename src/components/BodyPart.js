bc120a0bc2ad4381db1cee63d7b1abde520ad8a6
valensixx <valensixx@gmail.com>
2023-06-25 13:56:08 +0300

    fixing errors

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -2,23 +2,18 @@ import React from 'react';
 import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
 
-const BodyPart = ({item, setBodyPart, bodyPart}) => {
-  return (
+const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+  
    <Stack 
         type="button"
         alignItems="center"
         justifyContent="center"
         className="bodyPart-card"
-        sx = {{
-                borderTop: bodyPart === item ? '4px solid #FF2625' : '', 
-                borderTop: '4px solid #FF2625',
-                background: '#fff',
-                borderBottomLeftRadius: '20px',
-                width: '270px',
-                height: '280px',
-                cursor: 'pointer',
-                gap: '47px'
-            }}
+        sx={
+          bodyPart === item ? 
+          { borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } 
+          : 
+          { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
             onClick={() => {
                 setBodyPart(item);
                 window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
@@ -30,7 +25,7 @@ const BodyPart = ({item, setBodyPart, bodyPart}) => {
             {item}
         </Typography>
    </Stack>
-  )
-}
+  
+);
 
 export default BodyPart
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -29,6 +29,7 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
       }
       setExercises(exercisesData);
     }
+    fetchExercisesData();
   }, [bodyPart])
 
   return (
