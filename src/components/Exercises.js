f5eb1a2d10c3731b9f8362e8c44151ee32dd1780
valensixx <valensixx@gmail.com>
2023-06-25 14:38:19 +0300

    pagination fixed!

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,36 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import  Pagination  from '@mui/material/Pagination';
-import {Box, Stack, Typography} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
- 
-const Exercises = ({exercises, setExercises, bodyPart}) => {
 
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisePerPage = 9;
 
-
-
-  useEffect(()=> {
+  useEffect(() => {
     const fetchExercisesData = async () => {
       let exercisesData = [];
 
       if (bodyPart === 'all') {
-        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+        exercisesData = await fetchData(
+          'https://exercisedb.p.rapidapi.com/exercises',
+          exerciseOptions
+        );
       } else {
-        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
       }
       setExercises(exercisesData);
     };
 
     fetchExercisesData();
-
   }, [bodyPart]);
 
   //pagination part
   const indexOfLastExercise = currentPage * exercisePerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
-  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
 
   const paginate = (e, value) => {
     setCurrentPage(value);
@@ -38,35 +43,46 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
   };
 
   return (
-    <Box id= "exercises"
-      sx = {{mt: {lg:'110px'}}}
-      mt = '50px'
-      p = '20px'
+    <Box
+      id="exercises"
+      sx={{ mt: { lg: '110px' } }}
+      mt="50px"
+      p="20px"
     >
-      <Typography variant='h3' mb='46px' fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }}>
+      <Typography
+        variant="h3"
+        mb="46px"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+      >
         Showing Results
       </Typography>
-      <Stack direction='row'
-        sx={{gap: {lg: '110px', sx: '50px'}}}
-        flexWrap='wrap'
-        justifyContent='center'
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: '110px', sx: '50px' } }}
+        flexWrap="wrap"
+        justifyContent="center"
       >
-        {currentExercises.map((exercise, index)=> (
-          <ExerciseCard key={index} exercise={exercise} /> 
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
-      <Stack mt='100px' alignItems='center' sx={{ mt: { lg: '114px', xs: '70px' } }}>
-          {exercises.lenght > 9 && (
-            <Pagination 
-              color='standard'
-              shape='rounded'
-              defaultPage={1}
-              count={Math.ceil(exercises.length / exercisePerPage)}
-              page={currentPage}
-              onChange={paginate}
-              size='large'
-            />
-          )}
+      <Stack
+        mt="100px"
+        alignItems="center"
+        sx={{ mt: { lg: '114px', xs: '70px' } }}
+      >
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisePerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   );
